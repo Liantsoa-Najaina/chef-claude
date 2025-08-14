09629bdd37b7882fcc64e04e6632644b85bf1f11
Liantsoa-Najaina <najaina.andriamampianina@gmail.com>
2025-08-14 07:29:19 +0300

    feat: implement ingredient input form and display list

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,32 @@
-import type { JSX } from "react";
+import { useState, useRef , type JSX } from "react";
 
 export default function Main():JSX.Element {
+    const [ingredients, setIngredients] = useState<string[]>([]);
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const newIngredient = inputRef.current?.value;
+        newIngredient && setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
+        console.log("New ingredient added:", newIngredient);
+        console.log("Ingredients:", ingredients);
+
+        inputRef.current && (inputRef.current.value = "");
+    };
+
+    
+
     return (
         <main className="flex flex-col main">
-            <form className="flex justify-center gap-3 h-10">
+            <form className="flex justify-center gap-3 h-10" onSubmit={handleSubmit}>
                 <input 
                     aria-label="Add ingredient" 
                     placeholder="e.g onions"
                     type="text"
                     className="rounded-md border border-solid border-[#D1D5DB] w-64 input "
+                    name="Ingredient"
+                    ref={inputRef}
                 />
                 <button 
                     className="font-sans rounded-md border-0 bg-[#141413] text-[#FAFAF8] px-0.5 max-w-44 text-base min-w-36 grow relative before:content-['+'] before:mr-[5px]"
@@ -16,6 +34,9 @@ export default function Main():JSX.Element {
                     Add ingredient
                 </button>
             </form> 
+            <ul>
+                {ingredients.map(ingredient => (<li key={ingredient}>{ingredient}</li>))}
+            </ul>
         </main>
-    )
+    )  
 }
