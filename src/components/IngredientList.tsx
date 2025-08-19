52192bdccd8caafac711a83b1493ceed403f84c0
Liantsoa-Najaina <najaina.andriamampianina@gmail.com>
2025-08-20 00:04:19 +0300

    feat: add IngredientList component to display ingredients

---
+++ src/components/IngredientList.tsx
@@ -0,0 +1,22 @@
+import type { FC } from "react";
+
+type IngredientListProps = {
+    ingredients: string[];
+}
+
+const IngredientList: FC<IngredientListProps> = ({ ingredients }) => {
+    return (
+        <section className="flex flex-col items-center p-6 mt-4">
+            <h2 className="text-2xl font-[300] text-gray-800 mb-">Ingredients on hand:</h2>
+            <ul className="m-0 pl-5 space-y-2 list-disc">
+                {ingredients.map((ingredient, index) => (
+                    <li key={index} className="text-gray-600 hover:text-gray-800 transition-colors">
+                        {ingredient}
+                    </li>
+                ))}
+            </ul>
+        </section>
+    ) 
+}
+
+export default IngredientList
