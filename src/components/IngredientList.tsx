fba43f32db44fdd5cff0739c92253fcb5b84b95a
Liantsoa-Najaina <najaina.andriamampianina@gmail.com>
2025-08-20 00:36:20 +0300

    style(IngredientList): update header font weight for improved readability

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -7,7 +7,7 @@ type IngredientListProps = {
 const IngredientList: FC<IngredientListProps> = ({ ingredients }) => {
     return (
         <section className="flex flex-col items-center p-6 mt-4">
-            <h2 className="text-2xl font-[300] text-gray-800 mb-">Ingredients on hand:</h2>
+            <h2 className="text-2xl font-semibold text-gray-800 font-sans">Ingredients on hand:</h2>
             <ul className="m-0 pl-5 space-y-2 list-disc">
                 {ingredients.map((ingredient, index) => (
                     <li key={index} className="text-gray-600 hover:text-gray-800 transition-colors">
