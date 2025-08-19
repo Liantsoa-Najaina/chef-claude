f84b16bb2e324f8b161236240bc99c8c407aa06a
Liantsoa-Najaina <najaina.andriamampianina@gmail.com>
2025-08-20 00:30:16 +0300

    style(Header): make header sticky and adjust z-index for better visibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import chefClaudeIcon from '../images/ChefClaudeIcon.png'
 
 export default function Header(): JSX.Element {
     return (
-        <header className='flex items-center justify-center  shadow-md h-16 bg-white gap-2.5'>
+        <header className='flex items-center justify-center  shadow-md h-16 bg-white gap-2.5 sticky top-0 z-10'>
             <img src={chefClaudeIcon} alt="Chef Claude Icon" className='w-10 aspect-auto'/>
             <h1 className='text-center font-sans text-3xl font-normal text-gray-800'>Chef Claude</h1>
         </header>
