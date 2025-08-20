4c7bf3861c6d19c3dda2a4675e5714e1a2eb3d51
Liantsoa-Najaina <najaina.andriamampianina@gmail.com>
2025-08-20 03:43:26 +0300

    style: add animation upon mounting on the generate recipe card

--- src/components/GenerateRecipeCard.tsx
+++ src/components/GenerateRecipeCard.tsx
@@ -2,7 +2,7 @@ import type { JSX } from "react";
 
 export default function GenerateRecipeCard(): JSX.Element {
     return (
-        <div className="flex flex-col sm:flex-row justify-between items-center rounded-lg bg-[#F0EFEB] py-4 px-6 mt-6 gap-4">
+        <div className="flex flex-col sm:flex-row justify-between items-center rounded-lg bg-[#F0EFEB] py-4 px-6 mt-6 gap-4 animate-[slideInFade_0.5s_ease-out_forwards]">
             <div className="text-center sm:text-left">
                 <h3 className="text-[1.125rem]/[24px] font-medium">Ready for a recipe?</h3>
                 <p className="text-[#6B7280] text-[0.875rem]/[20px]">Generate a recipe from your list of ingredients.</p>
